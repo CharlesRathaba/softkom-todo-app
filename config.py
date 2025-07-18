d9631c91e65918f2be1a653c6944a33d2bbf0e82
Charles Rathaba <rathabacharles@gmail.com>
2025-07-18 13:51:08 +0200

    NeonDB connection

--- config.py
+++ config.py
@@ -4,6 +4,5 @@ basedir = os.path.abspath(os.path.dirname(__name__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', '%Vfsio19^k@')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
